4f882d6ca5f253d048499e988c5e773179723231
Marco <marco98x2@gmail.com>
2018-03-29 15:04:25 +0000

    created the project and defined the index route

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require("express"),
+    app = express();
+    port = process.env.PORT || 3000
+    
+    var todoRoutes = require("./routes/todos")
+    
+app.get("/", function(req, res){
+    res.send("Hi There from the root route!!!")
+});
+
+app.use("/api/todos", todoRoutes);
+
+
+    
+    
+app.listen(port, function(){
+    console.log("App is running on port "+ process.env.PORT)
+})
